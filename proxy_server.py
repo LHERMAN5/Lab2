3b46fd2502b9a0bd024f8c94aa95d44b72055320
Lauren Herman <thisisjustfabulous.0@gmail.com>
2023-09-27 19:13:50 -0600

    Update proxy_server.py

--- proxy_server.py
+++ proxy_server.py
@@ -4,75 +4,22 @@ import time
 from threading import Thread
 
 #define address & buffer size
-HOST = ""
-PORT = 8001
-BUFFER_SIZE = 1024
-
-def create_tcp_socket():
-    print('Creating socket')
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except (socket.error, msg):
-        print(f'Failed to create socket. Error code: {str(msg[0])} , Error message : {msg[1]}')
-        sys.exit()
-    print('Socket created successfully')
-    return s
-
-#get host information
-def get_remote_ip(host):
-    print(f'Getting IP for {host}')
-    try:
-        remote_ip = socket.gethostbyname( host )
-    except socket.gaierror:
-        print ('Hostname could not be resolved. Exiting')
-        sys.exit()
-
-    print (f'Ip address of {host} is {remote_ip}')
-    return remote_ip
-
-#send data to server
-def send_data(serversocket, payload):
-    print("Sending payload")    
-    try:
-        serversocket.sendall(payload.encode())
-    except socket.error:
-        print ('Send failed')
-        sys.exit()
-    print("Payload sent successfully")
+HOST = "127.0.0.1"
+PORT = 8080
+BUFFER_SIZE = 4096
 
 def send_request(host, port, request):
-    try:
-        #define address info, payload, and buffer size
-        host = 'www.google.com'
-        port = 80
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((host, port))
+        client_socket.send(request)
+        client_socket.shutdown(socket.SHUT_WR)
+        data = client_socket.recv(BUFFER_SIZE)
+        result = b'' + data
 
-        buffer_size = 4096
-
-        #make the socket, get the ip, and connect
-        s = create_tcp_socket()
-
-        remote_ip = get_remote_ip(host)
-
-        s.connect((remote_ip , port))
-        print (f'Socket Connected to {host} on ip {remote_ip}')
-        
-        #send the data and shutdown
-        send_data(s, request)
-        s.shutdown(socket.SHUT_WR)
-
-        #continue accepting data until no more left
-        full_data = b""
-        while True:
-            data = s.recv(buffer_size)
-            if not data:
-                 break
-            full_data += data
-        print(full_data)
-    except Exception as e:
-        print(e)
-    finally:
-        #always close at the end!
-        s.close()
+        while len(data) > 0:
+            data = client_socket.recv(BUFFER_SIZE)
+            result += data
+        return result
 
 def handle_connection(conn, addr):
     print("Connected by", addr)
@@ -110,9 +57,8 @@ def start_server():
         #QUESTION 3
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         
-        #bind socket to address
+
         s.bind((HOST, PORT))
-        #set to listening mode
         s.listen(2)
         
         conn, addr = s.accept()
